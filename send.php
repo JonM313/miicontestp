94103afbe1295b28b74592cc822072a126aaad97
Eridion Kiorai <erikiorai@gmail.com>
2018-03-26 15:14:07 +0300

    added send.php

---
+++ send.php
@@ -0,0 +1,15 @@
+<?php
+require config/config.php;
+
+$macaddres = $_GET['macaddres']
+$wiinumber = $_GET['wiinumber']
+$KeyVersion = $_GET['KeyVersion']
+$Version = $_GET['Version']
+$Server = $_GET['Server']
+$Country = $_GET['Country']
+$Group = $_GET['Group']
+$PopGroup = $_GET['PopGroup']
+$Language = $_GET['Language']
+$ConLanguage = $_GET['ConLanguage']
+$Entry = $_GET['Entry']
+$Chachedump = $_GET['Chachedump']
